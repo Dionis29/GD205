b4cf538e4334e49188ffda6b5f53d06089857f32
Dionis De Los Santos <dionissantos29@gmail.com>
2020-02-13 20:21:26 -0500

    First Code

---
+++ Assets/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 initPos;
+    public Transform[] hazard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+        //MOVEMENT CODE
+    {   if(Input.GetKeyDown(KeyCode.A))
+        {player.transform.position += new Vector3(0, 0, 1); }
+        if(Input.GetKeyDown(KeyCode.W))
+        {player.transform.position += new Vector3(1,0,0); }
+        if(Input.GetKeyDown(KeyCode.S))
+        {player.transform.position += new Vector3(-1,0,0);}
+        if(Input.GetKeyDown(KeyCode.D))
+        {player.transform.position += new Vector3(0,0,-1);}
+
+        //HAZARD CODE
+        for (int i=0; i<hazard.Length; i++)
+        if(hazard[i].position == player.transform.position)
+        {player.transform.position = initPos;}
+
+}
+}
