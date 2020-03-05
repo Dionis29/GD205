cc62642d9be5412f7e03b6f65155fab7170eba7d
Dionis29 <60491540+Dionis29@users.noreply.github.com>
2020-03-05 17:09:26 -0500

    Jungle Homework

---
+++ New Stuff/HunterMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterMovement : MonoBehaviour
+{
+    public GameObject HUNTER;
+    Vector3 initPos;
+
+    Transform cameraT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPos = HUNTER.transform.position;
+        cameraT = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if (Input.GetKeyDown(KeyCode.W))
+        { HUNTER.transform.position += new Vector3(1, 0, 0); }
+        if (Input.GetKeyDown(KeyCode.S))
+        { HUNTER.transform.position += new Vector3(-1, 0, 0); }
+        if (Input.GetKeyDown(KeyCode.A))
+        { HUNTER.transform.position += new Vector3(0, 0, 1); }
+        if (Input.GetKeyDown(KeyCode.D))
+        { HUNTER.transform.position += new Vector3(0, 0, -1); }
+
+
+
+    }
+}
---
+++ Thrust2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrust : MonoBehaviour
+{
+    Rigidbody rb;
+    public GameObject victim;
+
+    // Start is called before the first frame update
+    void Start()
+    { rb = GetComponent<Rigidbody>(); }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+
+
+
+    }
+
+    void OnCollisionEnter(Collision MycCollision)
+    {
+        if (MycCollision.gameObject == victim)
+        { Destroy(victim); }
+        print("lol collision");
+    }
+}
+
